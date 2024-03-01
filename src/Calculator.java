ce9d0ae10f848c7f3f971e88cd4daff1ab33fc25
Khasan <kyuldashev2000@icloud.com>
2024-03-01 16:22:49 -0500

    created Calculate method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,37 @@
 public class Calculator {
     public static void main(String[] args) {
 
+        int result =  calculate(5, '+', 10);
+        System.out.println(result);
+
     }
+
+    public static int calculate(int num1, char operator, int num2) {
+
+        int result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    throw new IllegalArgumentException("Cannot divide by zero");
+                }
+                result = num1 / num2;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+
+        return result;
+    }
+
 }
+
